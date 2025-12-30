eea8d275b6c7d45fabd20eb93ce6e7ff44cb21de
nikomaru4711 <nikowanngt@gmail.com>
2025-12-30 14:30:33 +0900

    型作成

---
+++ types/index.ts
@@ -0,0 +1,13 @@
+export type Recipe = {
+    id: string;
+    name: string;
+    url?: string;
+    ingredients: string;
+    created_at: string;
+}
+
+export type ingredients = {
+    id: string;
+    name: string;
+    created_at: string;
+}
