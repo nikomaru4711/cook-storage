dfc55956f11b6911a76dde7a46b88cf6a073432a
nikomaru4711 <nikowanngt@gmail.com>
2025-12-30 15:02:32 +0900

    Header作成

--- src/app/features/components/Header.tsx
+++ src/app/features/components/Header.tsx
@@ -1,7 +1,13 @@
-
+import Link from "next/link";
 
 export function Header() {
     return (
-        <div>Header Component</div>
+        <header className="app-header flex justify-between w-full py-3 bg-blue-400 ">
+            <Link className="ml-2" href="/">Cook Storage</Link>
+            <div className="flex gap-4 mx-2">
+                <Link href="/">料理一覧</Link>
+                <Link href="/">タグ一覧</Link>
+            </div>
+        </header>
     );
 }
