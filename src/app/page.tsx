4cf9cb18d19e07355b29a3902d2c2d215ef4527e
nikomaru4711 <nikowanngt@gmail.com>
2025-12-31 15:33:51 +0900

    useEffectの再レンダリング防止

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { getAllRecipes } from './features/recipe_crud.action';
 import { ToastTest } from './features/components/ToastTest';
 import { ToastContainer } from '@/app/features/components/Toast';
 import { useToast } from './features/hooks/useToast';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import type { Recipe } from '@/../types';
 
 export default function Home() {
@@ -12,7 +12,7 @@ export default function Home() {
   const { toasts, show, remove } = useToast();
 
 
-  const fetchData = async () => {
+  const fetchData = useCallback(async () => {
     try{
       const results = await getAllRecipes();
       if(!results.error){
@@ -23,12 +23,12 @@ export default function Home() {
       console.error("データの取得に失敗しました。", error);
       show('error', 'データの取得に失敗しました。', 4000);
     }
-  };
+  },[show]);
 
   //最初に一度だけデータ取得
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [fetchData]);
 
   return (
     <div>
