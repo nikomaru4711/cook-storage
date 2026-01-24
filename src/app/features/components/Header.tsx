e58d7fe0926dc40a14422d78aa873f844ae2592a
nikomaru4711 <nikowanngt@gmail.com>
2026-01-24 22:17:23 +0900

    実装しないものなのでコメントアウト

--- src/app/features/components/Header.tsx
+++ src/app/features/components/Header.tsx
@@ -5,8 +5,8 @@ export function Header() {
         <header className="app-header flex justify-between w-full py-3 bg-blue-400 ">
             <Link className="ml-2" href="/">Cook Storage</Link>
             <div className="flex gap-4 mx-2">
-                <Link href="/">料理一覧</Link>
-                <Link href="/">タグ一覧</Link>
+                {/* <Link href="/">料理一覧</Link> */}
+                {/* <Link href="/">タグ一覧</Link> */}
             </div>
         </header>
     );
